31b391c841751f9d98bbce55c38b183fcb60d802
Murilo Rosa <mrmurilo75@gmail.com>
2023-01-05 07:52:42 +0000

    fix typo in urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = 'blog'
-urlspatterns = [
+urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
