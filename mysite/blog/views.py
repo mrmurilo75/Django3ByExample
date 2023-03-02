8a7637edb1bc73223ead79dd06f11bc7c66da260
Murilo Rosa <mrmurilo75@gmail.com>
2023-03-02 23:34:42 +0000

    use generic ListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,22 +1,14 @@
-from django.core import paginator
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-
-    pagination = paginator.Paginator(posts, 3)
-    page = request.GET.get('page')
-    try:
-        posts = pagination.page(page)
-    except paginator.PageNotAnInteger:
-        posts = pagination.page(1)
-    except paginator.EmptyPage:
-        posts = pagination.page(pagination.num_pages)
-
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
