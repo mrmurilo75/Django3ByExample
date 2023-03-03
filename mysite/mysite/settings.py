2fb152f6378ad7b1574b5d8ab1080f1e6a4c69be
Murilo Rosa <mrmurilo75@gmail.com>
2023-03-03 00:23:38 +0000

    add sharing by email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
